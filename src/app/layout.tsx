6a28f66c321c951c02017f6696f2c14ad88e7aeb
Arttu Olli <arttu.olli@pinja.com>
2025-08-14 08:15:31 +0300

    Switch to Open Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,30 @@
 import "~/styles/globals.css";
 
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
 	title: "Sanamestari",
 	description: "Sanapeli",
-	icons: [{ rel: "icon", url: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🙂</text></svg>" }],
+	icons: [
+		{
+			rel: "icon",
+			url: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🙂</text></svg>",
+		},
+	],
 };
 
-const geist = Geist({
-	subsets: ["latin"],
-	variable: "--font-geist-sans",
+const openSans = Open_Sans({
+	subsets: ["latin-ext"],
+	variable: "--font-open-sans",
 });
 
 export default function RootLayout({
 	children,
 }: Readonly<{ children: ReactNode }>) {
 	return (
-		<html lang="fi" className={`${geist.variable}`}>
+		<html lang="fi" className={`${openSans.variable}`}>
 			<body>{children}</body>
 		</html>
 	);
